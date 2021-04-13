da7e32fcd10542be806d1c50171ee292f2ed15f6
Woonjang Ahn <hodol@woowahan.com>
2021-04-14 02:24:13 +0900

    add hibernate-reactive dependency

--- sandbox-base/build.gradle.kts
+++ sandbox-base/build.gradle.kts
@@ -1,3 +1,5 @@
 dependencies {
-
+    implementation("org.hibernate.reactive:hibernate-reactive-core:1.0.0.CR1")
+    implementation("io.vertx:vertx-mysql-client:3.9.6") // 최신버전은 4.x지만 Hibernate가 지원안함
+    implementation("org.hibernate:hibernate-validator:7.0.1.Final")
 }
